6e3a4ccd9a89fef639e53c6bf95886372e6608d3
apirukw-ai <apirukw@gmail.com>
2026-09-22 15:01:57 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ window.SUPABASE_URL = "https://utwmhcllhgmoqnfbmjwf.supabase.co";
 window.SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0d21oY2xsaGdtb3FuZmJtandmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwMzAyMjEsImV4cCI6MjEwNTYwNjIyMX0.5xguMqgJ7c_uzmJd5-2GikKgvtraJOCG9IZ40y5VNA8";
 
 // สร้าง Supabase Client กลางไว้เรียกใช้
-window.supabaseClient = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
+window.supabaseClient = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_KEY);
